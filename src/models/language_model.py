4e0200e9b93341aad2b49df0de6d9530bfae370d
Carlos Leal <carlos.frleal@gmail.com>
2024-01-13 14:33:47 -0300

    req 03

--- src/models/language_model.py
+++ src/models/language_model.py
@@ -1,4 +1,4 @@
-from src.models.abstract_model import AbstractModel
+from models.abstract_model import AbstractModel
 from database.db import db
 
 
@@ -8,3 +8,9 @@ class LanguageModel(AbstractModel):
     def __init__(self, data: dict) -> None:
         super().__init__(data)  # inicializa o construtor da classe pai
         # com o dict recebido
+
+    def to_dict(self) -> dict:
+        return {
+            "name": self.data["name"],
+            "acronym": self.data["acronym"]
+        }
