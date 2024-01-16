ed183a7c163d888c688047b9cb14651756c41727
Carlos Leal <carlos.frleal@gmail.com>
2024-01-15 21:35:12 -0300

    req06

--- src/controllers/language_controller.py
+++ src/controllers/language_controller.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, request, render_template
 from models.language_model import LanguageModel
+from models.history_model import HistoryModel
 from deep_translator import GoogleTranslator
 
 
@@ -24,30 +25,38 @@ def index():
     )
 
 
-# @language_controller.route("/", methods=["POST"])
-# def translate_text():
-#     languages = LanguageModel.list_dicts()
-#     text_to_translate = request.form.get("text-to-translate")
-#     # o segundo parametro é o que retorna caso a chave nao exista
-#     translate_from = request.form.get("translate-from")
-#     translate_to = request.form.get("translate-to")
+@language_controller.route("/", methods=["POST"])
+def translate_text():
+    languages = LanguageModel.list_dicts()
+    text_to_translate = request.form.get("text-to-translate")
+    # o segundo parametro é o que retorna caso a chave nao exista
+    translate_from = request.form.get("translate-from")
+    translate_to = request.form.get("translate-to")
 
-#     translated = GoogleTranslator(
-#         source=translate_from, target=translate_to,
-#         ).translate(text_to_translate)
+    HistoryModel(
+        {
+            "text_to_translate": text_to_translate,
+            "translate_from": translate_from,
+            "translate_to": translate_to
+        }
+    ).save()
 
-#     return render_template(
-#         "index.html",
-#         languages=languages,
-#         text_to_translate=text_to_translate,
-#         translate_from=translate_from,
-#         translate_to=translate_to,
-#         translated=translated,
-#     )
+    translated = GoogleTranslator(
+        source=translate_from, target=translate_to,
+        ).translate(text_to_translate)
+
+    return render_template(
+        "index.html",
+        languages=languages,
+        text_to_translate=text_to_translate,
+        translate_from=translate_from,
+        translate_to=translate_to,
+        translated=translated,
+    )
 
 
 @language_controller.route("/reverse", methods=["POST"])
-def reverse_language_translate():
+def reverse_text_translate():
     languages = LanguageModel.list_dicts()
     text_to_translate = request.form.get("text-to-translate")
     translate_from = request.form.get("translate-from")
