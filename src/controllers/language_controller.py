52d992e89fe16d83724c648e46bb2ea878308615
Carlos Leal <carlos.frleal@gmail.com>
2024-01-15 21:01:51 -0300

    req06

--- src/controllers/language_controller.py
+++ src/controllers/language_controller.py
@@ -3,33 +3,21 @@ from models.language_model import LanguageModel
 from deep_translator import GoogleTranslator
 
 
-language_controller = Blueprint("language_translate", __name__)
+language_controller = Blueprint("language_controller", __name__)
 
 
-@language_controller.route("/", methods=["GET"])
+@language_controller.route("/", methods=["GET", "POST"])
 def index():
     languages = LanguageModel.list_dicts()
-    text_to_translate = "O que deseja traduzir?"
-    translate_from = "pt"
-    translate_to = "en"
-    translated = "What do you want to translate?"
+    if request.method == "POST":
+        text_to_translate = request.form.get("text-to-translate")
+        translate_from = request.form.get("translate-from")
+        translate_to = request.form.get("translate-to")
 
-    return render_template(
-        "index.html",
-        languages=languages,
-        text_to_translate=text_to_translate,
-        translate_from=translate_from,
-        translate_to=translate_to,
-        translated=translated
-    )
-
-
-@language_controller.route("/", methods=["POST"])
-def translate_text():
-    text_to_translate = request.form.get("text_to_translate")
-    # o segundo parametro é o que retorna caso a chave nao exista
-    translate_from = request.form.get("translate_from")
-    translate_to = request.form.get("translate_to")
+    else:
+        text_to_translate = "O que deseja traduzir?"
+        translate_from = "pt"
+        translate_to = "en"
 
     translated = GoogleTranslator(
         source=translate_from, target=translate_to
@@ -37,8 +25,9 @@ def translate_text():
 
     return render_template(
         "index.html",
+        languages=languages,
         text_to_translate=text_to_translate,
         translate_from=translate_from,
         translate_to=translate_to,
-        translated=translated
+        translated=translated,
     )
